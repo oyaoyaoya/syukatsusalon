6dede10333e4dc92869a62efe188af303754ee37
makuri <m.change0525@gmail.com>
2016-03-05 21:13:02 +0900

    company追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'syukatsusalon/index'
   get 'syukatsusalon/top'
-  # The priority is based upon order of creation: first created -> highest priority.
+  get 'syukatsusalon/company' 
+ # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
 root to:'syukatsusalon#index'
